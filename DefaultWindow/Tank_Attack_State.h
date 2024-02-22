f98e5b35833d9ea96b03369e61048d6171971bac
MYOUNGSEUNGHO <myoung6458@naver.com>
2024-02-22 20:18:13 +0900

    이제 맵 추가할거임

--- DefaultWindow/Tank_Attack_State.h
+++ DefaultWindow/Tank_Attack_State.h
@@ -12,8 +12,7 @@ public:
     void Late_Update(CObj_Dynamic*) override;
     void Render(CObj_Dynamic*, HDC hDC) override;
     void Release(CObj_Dynamic*) override;
-
-    // IState을(를) 통해 상속됨
+public:
     void Move_Frame() override;
 };
 
